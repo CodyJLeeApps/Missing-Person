44c5bb9b3d3a192e06d5ecedba3b2688021779bc
Cody Lee <coduslee@bellsouth.net>
2016-06-15 18:42:16 -0500

    Initial Commit

---
+++ MissingPersons/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MissingPersons
+//
+//  Created by Cody Lee on 6/15/16.
+//  Copyright © 2016 CJLee Apps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
